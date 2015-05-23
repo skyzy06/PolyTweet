3e010981f8945be03352f0986807448a3598700d
Thomas Clop <clop@polytech.unice.fr>
2015-05-23 12:25:02 +0200

    it's working

--- PolyClient/src/PolyClient.java
+++ PolyClient/src/PolyClient.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package polyclient;
 
 /**
  *
@@ -17,5 +11,5 @@ public class PolyClient {
     public static void main(String[] args) {
         // TODO code application logic here
     }
-    
+
 }
